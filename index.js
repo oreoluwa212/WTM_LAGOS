9705dfd8bfe38718b8c3235dfaa2bf7041cbb731
oreoluwa212 <105554286+oreoluwa212@users.noreply.github.com>
2023-07-28 03:00:30 +0100

    JS basics

---
+++ index.js
@@ -0,0 +1,28 @@
+function calculate() {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  const operator = document.getElementById("operator").value;
+  let result;
+
+  switch (operator) {
+    case "add":
+      result = num1 + num2;
+      break;
+    case "subtract":
+      result = num1 - num2;
+      break;
+    case "multiply":
+      result = num1 * num2;
+      break;
+    case "divide":
+      result = num1 / num2;
+      break;
+    default:
+      result = "Invalid operator";
+  }
+
+  document.getElementById(
+    "result"
+  ).textContent = `The result is therefore equal to ${result}`;
+  window.alert(`The result is therefore equal to ${result}`)
+}
